900bcddfa37eafb771a730dade5160e9bfeb47a8
Ariel R-A <raarielgrace@gmail.com>
2020-01-16 13:28:03 -0500

    Changed front page css.

--- client/components/Video.jsx
+++ client/components/Video.jsx
@@ -8,13 +8,11 @@ const Video = props => {
   return (
     <div className="col-sm-4 video-card">
       <div className="video-content" onClick={() => toVideoPage(video_link)}>
-        <iframe src ={video_link}></iframe><br/>
-        <p>Location: {location}</p>
-        <form action={original_link} method="get" target="_blank">
-          <button type="submit" className="original-button">Original Video Link</button> 
-        </form>
-        <h2>{title}</h2>
-        <p>Location: {location}</p>
+        <div className="video-content-container">
+          <iframe src ={video_link}></iframe><br/>
+        </div>
+        <h2 class="video-title">{title}</h2>
+        <p class="subtitle">{location} | <a href={original_link}>Source</a></p>
         </div>
     </div>
   );
